49877cd9f8edbdf848881a4ac275797e78218b16
Nguyễn Trí Thành <nguyentrithanh2409@gmail.com>
2023-12-13 16:43:20 +0700

    Website Done

--- admin_page.php
+++ admin_page.php
@@ -37,17 +37,14 @@
       <div class="inner-section-one row">
         <div class="inner-name col-10">Admin Page</div>
         <div class="inner-user col-2">
-          <img src="assets/images/admin.jpg" alt="">
+          <img src="assets/images/fourteen.png" alt="" style="background-color: #EFD161;">
           <span class="admin">Admin</span>
         </div>
       </div>
       <div class="inner-section-two row align-items-center">
         <p class="admin-page col-10">Admin Page</p>
         <div class="col-2">
-          <a href="#" class="inner-export button">
-            <i class="fa-solid fa-cloud-arrow-down"></i>
-            <div class="export">Export</div>
-          </a>
+          
         </div>
       </div>
 
@@ -55,6 +52,7 @@
         table {
           border-collapse: collapse;
           width: 100%;
+          margin-bottom: 25px;
         }
 
         th,
@@ -65,7 +63,7 @@
         }
       </style>
       <?php
-      
+
 
       $servername = "localhost";
       $username = "root";
@@ -78,7 +76,7 @@
         die("Kết nối đến MySQL thất bại: " . $conn->connect_error);
       }
 
-      
+
 
       // Thực hiện thêm dữ liệu nếu có dữ liệu được gửi từ biểu mẫu
       if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -95,7 +93,7 @@
           $sql = "INSERT INTO listfinger (HoTen, QueQuan, Question1, Answer1, Question2, Answer2, Question3, Answer3) VALUES ('$hoTen', '$queQuan', '$question1', '$answer1', '$question2', '$answer2', '$question3', '$answer3')";
 
           if ($conn->query($sql) === TRUE) {
-            echo "Dữ liệu đã được thêm thành công!";
+            echo "Dữ liệu đã được thêm thành công!\n";
           } else {
             echo "Lỗi: " . $conn->error;
           }
@@ -105,7 +103,7 @@
           $sql = "DELETE FROM listfinger WHERE ID = $idToDelete";
 
           if ($conn->query($sql) === TRUE) {
-            echo "Dữ liệu đã được xoá thành công!";
+            echo "Dữ liệu đã được xoá thành công!\n";
           } else {
             echo "Lỗi: " . $conn->error;
           }
@@ -121,7 +119,20 @@
         echo "<tr><th>ID</th><th>Họ Tên</th><th>Quê Quán</th><th>Question 1</th><th>Answer 1</th><th>Question 2</th><th>Answer 2</th><th>Question 3</th><th>Answer 3</th><th>Action</th></tr>";
 
         while ($row = $result->fetch_assoc()) {
-          echo "<tr><td>" . $row["ID"] . "</td><td>" . $row["HoTen"] . "</td><td>" . $row["QueQuan"] . "</td><td>" . $row["Question1"] . "</td><td>" . $row["Answer1"] . "</td><td>" . $row["Question2"] . "</td><td>" . $row["Answer2"] . "</td><td>" . $row["Question3"] . "</td><td>" . $row["Answer3"] . "</td><td><form method='post'><input type='hidden' name='idToDelete' value='" . $row["ID"] . "'><input type='submit' name='delete' value='Xoá'></form></td></tr>";
+          echo "<tr><td>" . $row["ID"] .
+            "</td><td>" . $row["HoTen"] .
+            "</td><td>" . $row["QueQuan"] .
+            "</td><td>" . $row["Question1"] .
+            "</td><td>" . $row["Answer1"] .
+            "</td><td>" . $row["Question2"] .
+            "</td><td>" . $row["Answer2"] .
+            "</td><td>" . $row["Question3"] .
+            "</td><td>" . $row["Answer3"] .
+            "</td><td>
+          <form method='post'><input type='hidden' name='idToDelete' value='" . $row["ID"] . "'>
+          <input type='submit' name='delete' value='Xoá'>
+          </form>
+          </td></tr>";
         }
 
         echo "</table>";
@@ -129,46 +140,100 @@
         echo "Không có dữ liệu";
       }
 
+      if (isset($_POST['delete'])) {
+        $idToDelete = $_POST['idToDelete'];
+    
+        // Xoá hàng từ cơ sở dữ liệu
+        $sqlDelete = "DELETE FROM listfinger WHERE ID = $idToDelete";
+        $conn->query($sqlDelete);
+    
+        // Cập nhật ID cho các hàng còn lại
+        $sqlUpdateID = "SET @num := 0;
+                        UPDATE listfinger SET ID = @num := (@num + 1);
+                        ALTER TABLE listfinger AUTO_INCREMENT = 1;";
+        $conn->multi_query($sqlUpdateID);
+    }
+
       $conn->close();
+
+      
       ?>
 
-      <h2>Thêm Dữ Liệu</h2>
-      <form method="post">
-        <label for="hoTen">Họ Tên:</label>
-        <input type="text" name="hoTen" required>
-        <br>
 
-        <label for="queQuan">Quê Quán:</label>
-        <input type="text" name="queQuan" required>
-        <br>
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary button" data-toggle="modal" data-target="#exampleModal">
+        Add user
+      </button>
 
-        <label for="question1">Question 1:</label>
-        <input type="text" name="question1" required>
-        <br>
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Hãy điền đầy đủ thông tin theo mẫu dưới đây</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form method="post">
+              <div class="form-group">
+                <label for="hoTen">Họ Tên:</label>
+                <input type="text" class="form-control" name="hoTen" required>
+                <br>
+              <!-- </div> -->
 
-        <label for="answer1">Answer 1:</label>
-        <input type="text" name="answer1" required>
-        <br>
+              <!-- <div class="form-group"> -->
+                <label for="queQuan">Quê Quán:</label>
+                <input type="text" class="form-control" name="queQuan" required>
+                <br>
+              <!-- </div> -->
 
-        <label for="question2">Question 2:</label>
-        <input type="text" name="question2" required>
-        <br>
+              <!-- <div class="form-group"> -->
+                <label for="question1">Question 1:</label>
+                <input type="text" class="form-control" name="question1" required>
+                <br>
+              <!-- </div> -->
 
-        <label for="answer2">Answer 2:</label>
-        <input type="text" name="answer2" required>
-        <br>
+              <!-- <div class="form-group"> -->
+                <label for="answer1">Answer 1:</label>
+                <input type="text" class="form-control" name="answer1" required>
+                <br>
+              <!-- </div> -->
 
-        <label for="question3">Question 3:</label>
-        <input type="text" name="question3" required>
-        <br>
+              <!-- <div class="form-group"> -->
+                <label for="question2">Question 2:</label>
+                <input type="text" class="form-control" name="question2" required>
+                <br>
+              <!-- </div>   -->
 
-        <label for="answer3">Answer 3:</label>
-        <input type="text" name="answer3" required>
-        <br>
+              <!-- <div class="form-group"> -->
+                <label for="answer2">Answer 2:</label>
+                <input type="text" class="form-control" name="answer2" required>
+                <br>
+              <!-- </div> -->
 
-        <input type="submit" name="add" value="Thêm">
-      </form>
+              <!-- <div class="form-group"> -->
+                <label for="question3">Question 3:</label>
+                <input type="text" class="form-control" name="question3" required>
+                <br>
+              <!-- </div> -->
 
+              <!-- <div class="form-group"> -->
+                <label for="answer3">Answer 3:</label>
+                <input type="text" class="form-control" name="answer3" required>
+                <br>
+              </div>
+              
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="submit" name="add" class="btn btn-primary">Save changes</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
 
     </div>
   </div>
